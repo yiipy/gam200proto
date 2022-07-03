05d8eb65f696f4c9a9bd6fa10f7a0b46125d50db
yiipy <yipxiuhan@gmail.com>
2022-07-03 09:26:24 +0800

    character placeholder and movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x); // -1 = left, 1 = right
+        animator.SetFloat("Vertical", movement.y); // -1 = down, 1 = up
+        animator.SetFloat("Speed", movement.sqrMagnitude); // more than 0.01 = moving
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
