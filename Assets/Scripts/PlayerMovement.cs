b0f2c482ba014b8b01bde2025b56caaaf3f1b3dc
yiipy <yipxiuhan@gmail.com>
2022-07-09 13:13:49 +0800

    pick up and drop off system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject passenger1start;
+    public GameObject passenger1end;
+    public GameObject passenger1onboat;
+
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
@@ -19,6 +23,15 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x); // -1 = left, 1 = right
         animator.SetFloat("Vertical", movement.y); // -1 = down, 1 = up
         animator.SetFloat("Speed", movement.sqrMagnitude); // more than 0.01 = moving
+
+        if (passenger1start.activeInHierarchy == false && passenger1end.activeInHierarchy == false)
+        {
+            passenger1onboat.SetActive(true);
+        }
+        else
+        {
+            passenger1onboat.SetActive(false);
+        }
     }
 
     void FixedUpdate()
